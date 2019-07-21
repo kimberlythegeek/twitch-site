db03d43c953e966dc1259fdf591de6aba66658b2
Kimberly Sereduck <email@kimberly.codes>
2019-07-21 03:19:03 +0100

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+             <a class="navbar-brand site-brand" href="#"><span>kimberlythegeek</span></a>
+            </nav>
+        )
+    }
+}
+
+export default Header;
