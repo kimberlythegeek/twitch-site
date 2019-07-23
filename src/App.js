6f62c3686865737d065508a8e20213b9fda2107d
Kimberly <ksereduck@mozilla.com>
2019-07-23 14:41:25 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js'
+import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    isLive: false
+  }
+
+  componentDidMount() {
+    fetch('https://api.twitch.tv/helix/streams?user_id=36769016',
+      {
+        headers: {
+        'Client-ID': 'tyms13crm70tx7nlmlf6eo8k41a1eq'
+      }
+    })
+    .then(res => res.json())
+    .then(response => console.log(JSON.stringify(response))
+    .catch(error => console.error(error)));
+  }
+
+  streamIsLive() {
+    fetch('https://api.twitch.tv/helix/streams?user_id=36769016',
+      {
+        headers: {
+        'Client-ID': 'tyms13crm70tx7nlmlf6eo8k41a1eq'
+      }
+    })
+    .then(res => res.json())
+    .then(response => console.log(JSON.stringify(response))
+    .catch(error => console.error(error)));
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ReactTwitchEmbedVideo channel="kimberlythegeek" />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-             <a class="navbar-brand site-brand" href="#"><span>kimberlythegeek</span></a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand site-brand" href="http://www.kimberlythegeek.tv">
+                    <span>kimberlythegeek</span>
+                </a>
             </nav>
         )
     }
